4874ac2065ab29a2ebb1ca0fae7481f8f6254a31
Rost <coderost@yandex.ru>
2023-02-11 16:47:26 +0300

    Create principal's repo.

--- src/main/java/com/rost/repositories/PrincipalRepository.java
+++ src/main/java/com/rost/repositories/PrincipalRepository.java
@@ -1,11 +1,13 @@
 package com.rost.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.rost.models.Principal;
 
 @Repository
-public interface UserRepository extends JpaRepository<Principal, Long> {
-
+public interface PrincipalRepository extends JpaRepository<Principal, Long> {
+    Optional<Principal> findByName(String username);
 }
