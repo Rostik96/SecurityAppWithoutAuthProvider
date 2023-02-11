05b62e59f0571486bef9658edc1fc53434029186
Rost <coderost@yandex.ru>
2023-02-12 00:10:10 +0300

    Refactor SecurityConfig.

--- src/main/java/com/rost/config/SecurityConfig.java
+++ src/main/java/com/rost/config/SecurityConfig.java
@@ -3,13 +3,19 @@ package com.rost.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserDetailsService userDetailsServiceImpl;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
+                .userDetailsService(userDetailsServiceImpl)
                 .authorizeHttpRequests().anyRequest().authenticated()
                 .and().formLogin()
                 .and().build();
