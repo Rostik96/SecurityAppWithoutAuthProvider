42130ec96937f5f4b1aa235ade4078033101ab23
Rost <coderost@yandex.ru>
2023-02-11 22:42:28 +0300

    Add new method

--- src/main/java/com/rost/Application.java
+++ src/main/java/com/rost/Application.java
@@ -1,13 +1,23 @@
 package com.rost;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rost.services.UserService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	private final UserService userService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		userService.readAll().forEach(System.out::println);
+	}
 }
--- src/main/java/com/rost/controllers/HelloController.java
+++ src/main/java/com/rost/controllers/HelloController.java
@@ -1,6 +1,9 @@
 package com.rost.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,13 @@ public class HelloController {
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello world!");
+    }
+
+    @GetMapping("/show-user-info")
+    public ResponseEntity<String> showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        System.out.println(userDetails);
+        return ResponseEntity.ok(userDetails.toString());
     }
 }
